58b847a8ea75c34a4e70b3ecfec079e7fca59038
ayden <aydendiel@gmail.com>
2025-05-25 10:51:37 -0400

    feat: dataset pipeline

--- create_dataset.py
+++ create_dataset.py
@@ -1,18 +1,39 @@
+import os
 import sys
 import numpy as np
 from draw import get_drawn_image
 
+script_directory = os.path.dirname(os.path.realpath(__file__))
+image_filename = sys.argv[2] if len(sys.argv) >= 3 else "images.npy"
+label_filename = sys.argv[3] if len(sys.argv) >= 4 else "labels.txt"
+image_filepath = os.path.join(script_directory, image_filename)
+label_filepath = os.path.join(script_directory, label_filename)
+
 images = []
 labels = []
 
-count = 100
+if os.path.exists(image_filepath) and os.path.exists(label_filepath):
+    print("Loading image and label data...")
+    loaded_images = np.load(image_filepath)
+    images = list(loaded_images)
+    with open(label_filepath, 'r') as f:
+        labels = f.read().split('\n')
+
+total_count = 100
 canceled = False
 
+digit_count = 10
+
+print("Controls:")
+print("\t<esc> Save work and quit")
+print("\t<space> Save current digit and continue")
+print("\t<q> Reset canvas")
+
 try:
-    for i in range(count // 10):
-        for n in range(10):
-            print(f'Write a {n}')
-            img = get_drawn_image(f'Write "{n}"')
+    for i in range(len(images) // digit_count, total_count // digit_count):
+        for n in range(len(images) % digit_count, digit_count):
+            print(f'({i * digit_count + n} / {digit_count * total_count}) Char: {n}')
+            img = get_drawn_image(f'Write "{n}"!')
             if img is None:
                 canceled = True
                 print("Finished due to escape key! Saving labeled data")
@@ -22,15 +43,16 @@ try:
         if canceled:
             break
 except KeyboardInterrupt:
-    print('Finished due to KeyboardInterrupt! Saving labeled data...')
+    print('Cancelling due to KeyboardInterrupt!')
+    exit(1)
 
-image_filename = sys.argv[2] if len(sys.argv) >= 3 else "images.out"
-label_filename = sys.argv[3] if len(sys.argv) >= 4 else "labels.out"
-print(f'Saving images to {image_filename}')
+print(f'Saving images to {image_filepath}')
 images = np.vstack(images)
-np.save(image_filename, images)
+np.save(image_filepath, images)
 
-print(f'Saving labels to {label_filename}')
-with open(label_filename, 'w') as f:
-    for label in labels:
-        f.write(f'{label}\n')
+print(f'Saving labels to {label_filepath}')
+with open(label_filepath, 'w') as f:
+    for i in range(len(labels)):
+        f.write(str(labels[i]))
+        if i != len(labels) - 1:
+            f.write("\n")
